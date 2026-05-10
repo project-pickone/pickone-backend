9c3e61ffed73944ddcb52c8dfcb47e2b539bf79e
jochongs <joch2712@naver.com>
2026-05-10 15:56:02 +0900

    chore: Setup prisma tansaction decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ClsModule } from 'nestjs-cls';
+import { PrismaModule } from './common/modules/prisma/prisma.module';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { PrismaService } from './common/modules/prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ClsModule.forRoot({
+      plugins: [
+        new ClsPluginTransactional({
+          imports: [PrismaModule],
+          adapter: new TransactionalAdapterPrisma({
+            prismaInjectionToken: PrismaService,
+            sqlFlavor: 'postgresql',
+          }),
+        }),
+      ],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/common/modules/prisma/prisma.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client/extension';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super();
+  }
+}
