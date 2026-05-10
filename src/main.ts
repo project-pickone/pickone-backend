657e5f5f5873b5882d1a71740dd9f90de04de642
jochongs <joch2712@naver.com>
2026-05-10 15:53:19 +0900

    chore: Add prisma schema file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableShutdownHooks();
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(' '),
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.enableCors({});
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
