f35d81d6b54d39ab80fe452fae6ce9365c40396f
jochongs <joch2712@naver.com>
2026-05-13 15:53:04 +0900

    feat: Add ApiBearerAuth

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,6 +1,11 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../../api/auth/auth.guard';
 import { Exception } from './exception.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 export const Auth = () =>
-  applyDecorators(UseGuards(AuthGuard), Exception(401, 'no login'));
+  applyDecorators(
+    UseGuards(AuthGuard),
+    Exception(401, 'no login'),
+    ApiBearerAuth(),
+  );
